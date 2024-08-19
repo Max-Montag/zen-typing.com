fe8dc92e6d093982f89962a88d63bef8b1e6249a
MaMon94 <max.montag1994@gmail.com>
2024-08-20 00:33:12 +0200

    add close button

--- src/components/ResultsCard.js
+++ src/components/ResultsCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LiaTimesSolid } from "react-icons/lia";
 
 const ResultsCard = ({ isOpen, closePopup, resetTime, typedChars, typedWords, errors }) => {
   if (!isOpen) return null;
@@ -10,7 +11,13 @@ const ResultsCard = ({ isOpen, closePopup, resetTime, typedChars, typedWords, er
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-300 ease-in-out">
-      <div className="w-1/2 h-1/2 p-8 flex flex-col justify-center bg-gradient-to-br from-emerald-50 to-emerald-200 rounded-xl shadow-xl transform scale-95 transition-transform duration-300 ease-in-out">
+      <div className="relative w-1/2 h-1/2 p-8 flex flex-col justify-center bg-gradient-to-br from-emerald-50 to-emerald-200 rounded-xl shadow-xl transform scale-95 transition-transform duration-300 ease-in-out">
+        <button
+          onClick={handleClose}
+          className="absolute top-6 right-6 text-emerald-700 hover:text-emerald-900 transition-colors duration-300 ease-in-out"
+        >
+          <LiaTimesSolid className="w-8 h-8"/>
+        </button>
         <div className="text-center flex flex-col gap-4">
           <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-semibold text-emerald-700 mb-4">Ergebnisse</h2>
           <p className="text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-gray-700">Wörter: {typedWords}</p>
